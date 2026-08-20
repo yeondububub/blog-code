ad0cc5a5716188526e56132a9f1f04ea0740f53a
ydbb <yeondububub@gmail.com>
2026-08-20 20:59:11 +0900

    spring saga orchestreator 프로젝트 생성

---
+++ spring/spring-saga-orchestrator/src/test/java/com/example/springsagaorchestrator/SpringSagaOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsagaorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSagaOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
