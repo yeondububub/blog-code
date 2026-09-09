dd8149687d02b4e205d05bcd8ac03bef31a3f242
ydbb <yeondububub@gmail.com>
2026-09-09 17:23:36 +0900

    LiveActivityTutorials - 프로젝트 생성

---
+++ ios/LiveActivitytutorials/LiveActivitytutorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveActivityTutorials
+//
+//  Created by Junmo Sung on 9/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/LiveActivitytutorials/LiveActivitytutorials/LiveActivitytutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityTutorialsApp.swift
+//  LiveActivityTutorials
+//
+//  Created by Junmo Sung on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
