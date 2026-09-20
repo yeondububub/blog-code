067934cd063729053783252992c14ed08b03f9ac
ydbb <yeondububub@gmail.com>
2026-09-20 18:48:11 +0900

    CombineTutorials - Subscription - Subscription을 cancel해야 하는 이유(View 세팅)

---
+++ ios/CombineTutorials/05-Subscription/02cancellable_cancel/02cancellable_cancel/ThirdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.swift
+//  02cancellable_cancel
+//
+//  Created by Junmo Sung on 9/20/26.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ThirdVC is loaded")
+    }
+}
---
+++ ios/CombineTutorials/05-Subscription/02cancellable_cancel/02cancellable_cancel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02cancellable_cancel
+//
+//  Created by Junmo Sung on 9/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
