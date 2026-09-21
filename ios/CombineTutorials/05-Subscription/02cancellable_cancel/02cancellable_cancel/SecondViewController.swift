3f63868a63a6730d4b429c2995dc6214a38231ee
ydbb <yeondububub@gmail.com>
2026-09-21 21:56:00 +0900

    CombineTutorials - Subscription - Subscription을 cancel해야 하는 이유

--- ios/CombineTutorials/05-Subscription/02cancellable_cancel/02cancellable_cancel/SecondViewController.swift
+++ ios/CombineTutorials/05-Subscription/02cancellable_cancel/02cancellable_cancel/SecondViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import Combine
 
 class SecondViewController: UIViewController {
+    
+    var cancellables: Set<AnyCancellable> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("SecondVC is loaded")
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { date in
+                let formatter = DateFormatter()
+                formatter.dateFormat = "HH:mm:ss:SSSSS"
+                let dateString = formatter.string(from: date)
+                print("current time: \(dateString)")
+            }
+            .store(in: &cancellables)
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cancellables.forEach { cancellable in
+            cancellable.cancel()
+        }
+        
+        //cancellables.removeAll()
+    }
+    
+    deinit {
+        print("SecondVC is deinitialized")
+    }
 }
